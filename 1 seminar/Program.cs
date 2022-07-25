fe3c94a8f019bec9a3338dcee19709dbc7d90896
Zhunus <samsungus071@gmail.com>
2022-07-25 13:07:21 +0600

    add new codes

---
+++ 1 seminar/Program.cs
@@ -0,0 +1,52 @@
+﻿
+// bool b = false;
+
+// multiplication
+// Console.Write("input number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int square = num * num;  
+
+// type - double and byte
+// double d = 5.44;
+// int i = 2022;
+// byte b = 155;
+// b = (byte)i;
+// Console.WriteLine(b);
+// Console.Write($"Square of {num} = {square}");
+
+
+
+
+// exponentiation
+// Console.Write("input number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// double square = Math.Pow(num, 5);
+// Console.Write($"Square of {num} = {square}");
+
+// Console.Write("input number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("input number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int square = num1 * num2;
+// if (num1 == num2 * num2)
+// {
+//     Console.WriteLine(true);
+// }
+// else Console.WriteLine (square);
+
+
+// comand #switch - case#
+// Console.Write("input number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// switch (num)
+// {
+//     case 1: Console.WriteLine("Monday"); break;
+//     case 2: Console.WriteLine("Thuesday"); break;
+//     case 3: Console.WriteLine("Wednesday"); break;
+//     case 4: Console.WriteLine("Thursday"); break;
+//     case 5: Console.WriteLine("Friday"); break;
+//     case 6: Console.WriteLine("Saturday"); break;
+//     case 7: Console.WriteLine("Sunday"); break;
+//     default: Console.WriteLine("Not exist"); break;
+// }

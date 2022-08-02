5b4bdfe61f9855ded726c0f2a5dbf13b601cd055
Zhunus <samsungus071@gmail.com>
2022-08-02 23:29:01 +0600

    add new tasks

---
+++ 1 task/Program.cs
@@ -0,0 +1,78 @@
+﻿
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Task 2
+// Console.Write("input number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("input number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if ( num1 <= num2 )
+// { 
+//     Console.WriteLine($"max = {num2}");    
+// }
+// else 
+// {
+//     Console.WriteLine($"max = {num1}");
+// }
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и 
+// выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Task 4
+
+// Console.Write("input number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("input number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("input number: ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+// int max = num1;
+// if (num1 > max) max = num1;
+// if (num2 > max) max = num2;
+// if (num3 > max) max = num3;
+//Console.Write(max);
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Task 6
+// Console.Write("input number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// int result = num1 % 2;
+
+// if (result > 0)
+// {
+//     Console.WriteLine($"odd number");
+// }
+// else Console.WriteLine($"even number");
+
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("input number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 1;
+int result = a / 2;
+while ( a < 1) 
+{
+   
+}
+Console.WriteLine(result);
+
+
